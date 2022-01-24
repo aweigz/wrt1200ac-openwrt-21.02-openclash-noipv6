35944cb226e06b19ccf9c31dab29d8b814eb9e35
aweigz <aweigz@gmail.com>
2022-01-25 05:52:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/kenzok8/small-package.git package/kenzok8
